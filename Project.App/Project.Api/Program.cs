2c3b669004dd5c14273821a8ab556a4a18d72afd
Salvarez99 <stephenalvarez3@gmail.com>
2025-10-13 12:54:05 -0400

    Api Structure Set Up

---
+++ Project.App/Project.Api/Program.cs
@@ -0,0 +1,46 @@
+using Serilog;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
+
+        //Auto Mapper
+        builder.Services.AddAutoMapper(typeof(Program));
+
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
