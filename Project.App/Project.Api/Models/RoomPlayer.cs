f02264bb143486863774f521bd7e99828cd3fe40
Salvarez99 <stephenalvarez3@gmail.com>
2025-10-15 15:32:42 -0400

    Fix build issues

--- Project.App/Project.Api/Models/RoomPlayer.cs
+++ Project.App/Project.Api/Models/RoomPlayer.cs
@@ -1,24 +1,27 @@
-namespace Project.Api.Models;
+
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Project.Api.Models
 {
-    [Key]
-    public long Id { get; set; }
+    public class RoomPlayer{
+        [Key]
+        public long Id { get; set; }
 
-    [Required]
-    public Guid RoomId { get; set; }
+        [Required]
+        public Guid RoomId { get; set; }
 
-    [Required]
-    public int UserId { get; set; }
+        [Required]
+        public int UserId { get; set; }
 
-    [ForeignKey("RoomId")]
-    public virtual Room? Room { get; set; }
+        [ForeignKey("RoomId")]
+        public virtual Room? Room { get; set; }
 
-    [ForeignKey("UserId")]
-    public virtual User? User { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User? User { get; set; }
 
-    public virtual ICollection<Hand> Hands { get; set; } = [];
+        public virtual ICollection<Hand> Hands { get; set; } = [];
+
+    }
 }
