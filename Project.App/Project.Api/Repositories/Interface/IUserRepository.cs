10ef0e4231dba4fac8568d044788dfe17f2f5367
Sneha013084 <snehayur30@gmail.com>
2025-10-16 12:49:34 -0400

    Add UserRepository interface and implementation

--- Project.App/Project.Api/Repositories/Interface/IUserRepository.cs
+++ Project.App/Project.Api/Repositories/Interface/IUserRepository.cs
@@ -1,8 +1,6 @@
 using Project.Api.Models;
 
-namespace Project.Api.Repositories
-{
-    public interface IUserRepository
+namespace Project.Api.Repositories.Interface
     {
         public interface IUserRepository
         {
@@ -14,63 +12,7 @@ namespace Project.Api.Repositories
             Task DeleteAsync(int id);
             Task SaveChangesAsync();
         }
-
-        // implementation
-
-        public class UserRepository : IUserRepository
-        {
-            private readonly AppDbContext _context;   // rename AppDbContext later 
-
-            public UserRepository(AppDbContext context)
-            {
-                _context = context;
-            }
-
-            // Get all users
-            public async Task<IEnumerable<User>> GetAllAsync()
-            {
-                return await _context.Users.ToListAsync();
-            }
-
-            //  Get user by ID
-            public async Task<User?> GetByIdAsync(int id)
-            {
-                return await _context.Users.FindAsync(id);
-            }
-
-            //  Get user by email
-            public async Task<User?> GetByEmailAsync(string email)
-            {
-                return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
-            }
-
-            // Add new user
-            public async Task AddAsync(User user)
-            {
-                await _context.Users.AddAsync(user);
-            }
-
-            // Update existing user
-            public async Task UpdateAsync(User user)
-            {
-                _context.Users.Update(user);
-            }
-
-            // Delete user by ID
-            public async Task DeleteAsync(int id)
-            {
-                var user = await _context.Users.FindAsync(id);
-                if (user != null)
-                {
-                    _context.Users.Remove(user);
-                }
-            }
-
-            //  Save changes
-            public async Task SaveChangesAsync()
-            {
-                await _context.SaveChangesAsync();
-            }
-        }
     }
-}
+
+
+        
---
+++ Project.App/Project.Api/Repositories/Interface/UserRepository.cs
@@ -0,0 +1,67 @@
+using Project.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Project.Api.Data;
+using Project.Api.Repositories.Interface;
+
+namespace Project.Api.Repositories
+{
+    
+      public class UserRepository : IUserRepository
+        {
+            private readonly AppDbContext _context;   // rename AppDbContext later 
+
+            public UserRepository(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            // Get all users
+            public async Task<IEnumerable<User>> GetAllAsync()
+            {
+                return await _context.Users.ToListAsync();
+            }
+
+            //  Get user by ID
+            public async Task<User?> GetByIdAsync(int id)
+            {
+                return await _context.Users.FindAsync(id);
+            }
+
+            //  Get user by email
+            public async Task<User?> GetByEmailAsync(string email)
+            {
+                return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            }
+
+            // Add new user
+            public async Task AddAsync(User user)
+            {
+                await _context.Users.AddAsync(user);
+            }
+
+            // Update existing user
+            public async Task UpdateAsync(User user)
+            {
+                _context.Users.Update(user);
+            }
+
+            // Delete user by ID
+            public async Task DeleteAsync(int id)
+            {
+                var user = await _context.Users.FindAsync(id);
+                if (user != null)
+                {
+                    _context.Users.Remove(user);
+                }
+            }
+
+            //  Save changes
+            public async Task SaveChangesAsync()
+            {
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+
+
