a6455cf03a1ff95057f167da0b8e4e02a81b8b6a
ArielFSoler <asole030@fiu.edu>
2025-10-22 08:31:19 -0700

    feat: Added Room Service(Missing Game Functionality)

---
+++ Project.App/Project.Api/DTOs/RoomDTOs/RoomDTO.cs
@@ -0,0 +1,17 @@
+namespace Project.Api.DTOs;
+
+public class RoomDTO
+{
+    public Guid Id { get; set; }
+    public Guid HostId { get; set; }
+    public bool IsPublic { get; set; }
+    public string GameMode { get; set; } = string.Empty;
+    public string GameState { get; set; } = string.Empty;
+    public string GameConfig { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public int MaxPlayers { get; set; }
+    public int MinPlayers { get; set; }
+    public string DeckId { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public bool IsActive { get; set; }
+}
